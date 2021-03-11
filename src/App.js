2345c27abddf67d8859b896150626d3e1c1488e1
王泽龙 <tom@wangzelongdeMacBook-Pro.local>
2021-03-11 23:43:44 +0800

    初始化redux以及引入

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React from "react";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./style";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import routes from "./routes";
 import { renderRoutes } from "react-router-config";
 import { HashRouter } from "react-router-dom";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <HashRouter>
         <GlobalStyle></GlobalStyle>
         <IconStyle></IconStyle>
         {renderRoutes(routes)}
       </HashRouter>
-    </div>
+    </Provider>
   );
 }
 
