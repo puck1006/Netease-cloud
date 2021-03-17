d7ab2dd42e35ad4fc8587a6bd321f4b97468df34
王泽龙 <tom@wangzelongdeMacBook-Pro.local>
2021-03-17 23:35:58 +0800

    轮播图完成~

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Slider from "../../components/slider/slider.jsx";
+import { Content } from "./style";
 
-export default function Recommend() {
-    return (
-        <div>
-            this is recommend component
-        </div>
-    )
+function Recommend() {
+  const bannerList = [1, 2, 3, 4].map((item) => {
+    return {
+      imageUrl:
+        "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg",
+    };
+  });
+
+  return (
+    <Content>
+      <div>
+        <Slider bannerList={bannerList}></Slider>
+      </div>
+    </Content>
+  );
 }
+
+export default React.memo(Recommend);
