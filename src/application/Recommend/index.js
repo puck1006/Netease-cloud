6f3bb79799f268353efde7123139b146cfe254e0
王泽龙 <tom@wangzelongdeMacBook-Pro.local>
2021-04-08 21:46:03 +0800

    大更新

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,68 +1,64 @@
-import React, { useEffect } from "react";
-import Slider from "../../components/slider/slider.jsx";
-import { Content } from "./style";
+import React, { useEffect } from 'react';
+import Slider from '../../components/slider/';
 import { connect } from "react-redux";
-import { getBannerList, getRecommendList } from "./store/actionCreators";
-import RecommendList from "../../components/list/index.jsx";
-import Loading from "../../baseUI/loading/index";
-import Scroll from "../../baseUI/scroll";
 import {forceCheck} from 'react-lazyload';
+import * as actionTypes from './store/actionCreators';
+import RecommendList from '../../components/list/';
+import Scroll from '../../baseUI/scroll/index';
+import { Content } from './style';
+import Loading from '../../baseUI/loading/index';
+import { renderRoutes } from 'react-router-config';
 
-function Recommend(props) {
+function Recommend(props){
   const { bannerList, recommendList, enterLoading } = props;
-  const { getRecommendListDataDispatch, getBannerDataDispatch } = props;
+
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+
   useEffect(() => {
-    if (!bannerList.size) {
+    if(!bannerList.size){
       getBannerDataDispatch();
     }
-    if (!recommendList.size) {
+    if(!recommendList.size){
       getRecommendListDataDispatch();
     }
+    // eslint-disable-next-line
   }, []);
-  const bannerListJS = bannerList ? bannerList.toJS() : [];
-  const recommendListJS = recommendList ? recommendList.toJS() : [];
 
-  const pullDown = () => {
-    console.log("pullDown");
-  };
-  const pullUp = () => {
-    console.log("pullUp");
-  };
-  
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS() :[];
 
   return (
     <Content>
-      <Scroll pullDown={pullDown} pullUp={pullUp} onScroll={forceCheck}>
+      <Scroll onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
-      {enterLoading ? <Loading></Loading> : ``}
-    </Content>
+      { enterLoading ? <Loading></Loading> : null }
+      { renderRoutes(props.route.routes) }
+    </Content> 
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    bannerList: state.getIn(["recommend", "bannerList"]),
-    recommendList: state.getIn(["recommend", "recommendList"]),
-    enterLoading: state.getIn(["recommend", "enterLoading"]),
-  };
-};
-
+// 映射Redux全局的state到组件的props上
+const mapStateToProps = (state) => ({
+  // 不要再这里将数据toJS,不然每次diff比对props的时候都是不一样的引用，还是导致不必要的重渲染, 属于滥用immutable
+  bannerList: state.getIn(['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn(['recommend', 'enterLoading'])//简单数据类型不需要调用toJS
+});
+// 映射dispatch到props上
 const mapDispatchToProps = (dispatch) => {
   return {
     getBannerDataDispatch() {
-      dispatch(getBannerList());
+      dispatch(actionTypes.getBannerList());
     },
     getRecommendListDataDispatch() {
-      dispatch(getRecommendList());
+      dispatch(actionTypes.getRecommendList());
     },
-  };
+  }
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(React.memo(Recommend));
+// 将ui组件包装成容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 import { Redirect } from "react-router-dom";
-import Home from "../application/Home";
-import Rank from "../application/Rank";
-import Recommend from "../application/Recommend";
-import Singers from "../application/Singers";
+import Recommend from '../application/Recommend';
+import Singers from '../application/Singers';
+import Rank from '../application/Rank';
+import Home from '../application/Home';
+import Album from '../application/Album';
 
-const routes = [
+export default [
   {
     path: "/",
     component: Home,
@@ -13,22 +14,35 @@ const routes = [
       {
         path: "/",
         exact: true,
-        render: () => <Redirect to={"/recommend"} />,
+        render: () => (
+          <Redirect to={"/recommend"}/>
+        )
       },
       {
-        path: "/recommend",
+        path: "/recommend/",
         component: Recommend,
+        routes: [
+          {
+            path: "/recommend/:id",
+            component: Album
+          }
+        ]
       },
       {
         path: "/singers",
-        component: Singers,
+        component: Singers
       },
       {
-        path: "/rank",
+        path: "/rank/",
         component: Rank,
+        key: "rank",
+        routes: [
+          {
+            path: "/rank/:id",
+            component: Album
+          }
+        ]
       },
-    ],
-  },
-];
-
-export default routes
+    ]
+  }
+]
